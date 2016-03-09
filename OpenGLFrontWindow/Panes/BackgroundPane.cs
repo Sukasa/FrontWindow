32d77af1350843bf0f1f7fbfcedcf02980a7d467
Sukasa <nick.m@shaw.ca>
2016-03-09 13:44:50 -0800

    Update background plane for proper rendering

--- OpenGLFrontWindow/Panes/BackgroundPane.cs
+++ OpenGLFrontWindow/Panes/BackgroundPane.cs
@@ -1,12 +1,8 @@
-﻿using System;
+﻿using Astronomy;
+using OpenGLFrontWindow.Properties;
 using OpenTK;
 using OpenTK.Graphics.OpenGL;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Astronomy;
-using System.Threading.Tasks;
-using OpenGLFrontWindow.Properties;
+using System;
 
 namespace OpenGLFrontWindow.Panes
 {
@@ -17,7 +13,7 @@ namespace OpenGLFrontWindow.Panes
         int Texture;
         float LerpAmt = 0;
 
-        public override bool Init()
+        protected internal override bool Init()
         {
             Vertexes = new Vertex[12];
 
@@ -43,7 +39,7 @@ namespace OpenGLFrontWindow.Panes
             return true;
         }
 
-        public override void Tick()
+        protected internal override void Tick()
         {
 
             Twilight2 T = new Twilight2();
@@ -71,7 +67,7 @@ namespace OpenGLFrontWindow.Panes
             }
             else if (Math.Abs(Current - Sunset) <= 30)
             {
-                int Test = Current - (Sunrise - 30);
+                int Test = Current - (Sunset - 30);
                 LerpAmt = 1 - ((float)Test / 60);
             }
             else
@@ -79,30 +75,22 @@ namespace OpenGLFrontWindow.Panes
                 LerpAmt = 0;
             }
 
+            Vector4 Colour1Day = new Vector4(0.171875f, 0.37109375f, 0.92578125f, 1.0f);
+            Vector4 Colour1Night = new Vector4(0.0f, 0.0f, 0.0f, 1.0f);
+
+            Vector4 Colour2Day = new Vector4(0.3828125f, 0.765625f, 0.95703125f, 1.0f);
+            Vector4 Colour2Night = new Vector4(0.11328125f, 0.01953125f, 0.1015625f, 1.0f);
+
+            Vector4 Colour3Day = new Vector4(0.23828125f, 0.81640625f, 0.94921875f, 1.0f);
+            Vector4 Colour3Night = new Vector4(0.19921875f, 0.0859375f, 0.19140625f, 1.0f);
+
             // Lerp all positions and colours from day to night (or back)
-            float Position1 = Lerp(56, 76, LerpAmt);
-            float Position2 = Lerp(20, 30, LerpAmt);
+            float Position1 = Lerp(56f, 76f, LerpAmt);
+            float Position2 = Lerp(20f, 30f, LerpAmt);
 
-            float Colour1r = Lerp(0, 44, LerpAmt);
-            float Colour1g = Lerp(0, 95, LerpAmt);
-            float Colour1b = Lerp(0, 237, LerpAmt);
-
-            float Colour2r = Lerp(29, 98, LerpAmt);
-            float Colour2g = Lerp(5, 196, LerpAmt);
-            float Colour2b = Lerp(26, 245, LerpAmt);
-
-            float Colour3r = Lerp(51, 65, LerpAmt);
-            float Colour3g = Lerp(22, 209, LerpAmt);
-            float Colour3b = Lerp(49, 243, LerpAmt);
-
-            // Assemble colour vectors
-            Vector4 Colour1 = new Vector4(Colour1r, Colour1g, Colour1b, 256.0f);
-            Vector4 Colour2 = new Vector4(Colour2r, Colour2g, Colour2b, 256.0f);
-            Vector4 Colour3 = new Vector4(Colour3r, Colour3g, Colour3b, 256.0f);
-
-            Colour1 /= 256f;
-            Colour2 /= 256f;
-            Colour3 /= 256f;
+            Vector4 Colour1 = Colour1Night + (Colour1Day - Colour1Night) * LerpAmt;
+            Vector4 Colour2 = Colour2Night + (Colour2Day - Colour2Night) * LerpAmt;
+            Vector4 Colour3 = Colour3Night + (Colour3Day - Colour3Night) * LerpAmt;
 
             // Now set vertex heights/colours
             Vertexes[6].Position.Y = Vertexes[7].Position.Y = Vertexes[8].Position.Y = Vertexes[9].Position.Y = Program.Configuration.Height * Position1 / 100;
@@ -114,12 +102,12 @@ namespace OpenGLFrontWindow.Panes
             Vertexes[6].Color = Vertexes[7].Color = Vertexes[8].Color = Vertexes[9].Color = Vertexes[10].Color = Vertexes[11].Color = Colour1;
         }
 
-        public override void Event(dynamic EventData)
+        protected internal override void Event(STOMP.StompMessageEventArgs EventData)
         {
             // Do nothing - background doesn't give a crap
         }
 
-        public override void Render()
+        protected internal override void Render()
         {
             BindTexture(Texture);
             UpdateVertices();
